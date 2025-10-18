72a5be0ad5dc8856ffce8a2fd69161a97f7b60b0
Rakesh Nair <crucifyrakesh@gmail.com>
2025-10-18 20:03:51 +0530

    convert to Signals

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from '../investment-input.model';
 
@@ -11,25 +11,24 @@ import type { InvestmentInput } from '../investment-input.model';
 })
 export class UserInputComponent {
 
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration= '10';
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
-  @Output() calculate = new EventEmitter<InvestmentInput>();
+  calculate = output<InvestmentInput>();
 
   onSubmit() {
-    console.log('Form submitted!');
-    console.log('Initial Investment:', this.enteredInitialInvestment);
-    console.log('Annual Investment:', this.enteredAnnualInvestment);
-    console.log('Expected Return:', this.enteredExpectedReturn);
-    console.log('Duration:', this.enteredDuration);
     this.calculate.emit({
-      initialInvestment: +this.enteredInitialInvestment,
-      annualInvestment: +this.enteredAnnualInvestment,
-      expectedReturn: +this.enteredExpectedReturn,
-      duration: +this.enteredDuration
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration()
     });
+  this.enteredInitialInvestment.set('0');
+  this.enteredAnnualInvestment.set('0');
+  this.enteredExpectedReturn.set('5');
+  this.enteredDuration.set('10');
   }
 
 }
